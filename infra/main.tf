f8d5a5ef81e989c167d0360f227a7b1da718bcc2
lebrizuela-deh <lbrizuela@externos.dehonline.es>
2025-05-15 13:16:12 -0300

    update

--- infra/main.tf
+++ infra/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "main-vpc" }
+  tags = { Name = "${var.environment}-vpc" }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = { Name = "main-igw" }
+  tags   = { Name = "${var.environment}-igw" }
 }
 
 resource "aws_subnet" "public_a" {
@@ -19,25 +19,25 @@ resource "aws_subnet" "public_a" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-  tags = { Name = "public-subnet-a" }
+  tags                    = { Name = "${var.environment}-public-subnet-a" }
 }
 
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
-  tags = { Name = "private-subnet-a" }
+  tags              = { Name = "${var.environment}-private-subnet-a" }
 }
 
 resource "aws_eip" "nat" {
   domain = "vpc"
+  tags   = { Name = "${var.environment}-eip-nat" }
 }
-
 
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_a.id
-  tags = { Name = "main-natgw" }
+  tags          = { Name = "${var.environment}-natgw" }
 }
 
 resource "aws_route_table" "public" {
@@ -46,7 +46,7 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = { Name = "public-rt" }
+  tags = { Name = "${var.environment}-public-rt" }
 }
 
 resource "aws_route_table" "private" {
@@ -55,7 +55,7 @@ resource "aws_route_table" "private" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.natgw.id
   }
-  tags = { Name = "private-rt" }
+  tags = { Name = "${var.environment}-private-rt" }
 }
 
 resource "aws_route_table_association" "public_a" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,8 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "environment" {
+  description = "Nombre del entorno (dev, qa, predev, prod)"
+  type        = string
+}
